0ab1ffcc58ed9963b088b9dfc3b9a6bc56bea5ad
Anatolii Zubenko <anatolii.zubenko@gmail.com>
2024-05-05 02:46:32 -0700

    added backend call and input hook

--- src/components/EffectSection.jsx
+++ src/components/EffectSection.jsx
@@ -1,19 +1,33 @@
-import { useState } from "react";
+import { useEffect, useCallback, useState } from "react";
 import Button from "./Button/Button";
 import Modal from "./Modal/Modal";
+import useInput from "../hooks/useinput";
 
 export default function EffectSection() {
-  const [modal, setModal] = useState();
+  const input = useInput();
+  const [modal, setModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
 
-  function openModal() {
-    setModal(true);
-  }
+  const fetchUsers = useCallback(async () => {
+    setLoading(true);
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
+    setUsers(users);
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchUsers();
+  }, [fetchUsers]);
 
   return (
     <section>
       <h3>Effects</h3>
 
-      <Button onClick={openModal}>Open info</Button>
+      <Button style={{ marginBottom: "1rem" }} onClick={() => setModal(true)}>
+        Open info
+      </Button>
 
       <Modal open={modal}>
         <h3>Hello from modal</h3>
@@ -24,8 +38,25 @@ export default function EffectSection() {
           Labore, fuga nulla? Voluptatem, error nihil. Aperiam possimus facilis
           sequi in!
         </p>
-        <Button onClick={() =>  setModal(false)}>Close modal</Button>
+        <Button onClick={() => setModal(false)}>Close modal</Button>
       </Modal>
+
+      {loading && <p>Loading...</p>}
+
+      {!loading && (
+        <>
+          <input type="text" className="control" {...input} />
+          <ul>
+            {users
+              .filter((user) =>
+                user.name.toLowerCase().includes(input.value.toLowerCase())
+              )
+              .map((user) => (
+                <li key={user.id}>{user.name}</li>
+              ))}
+          </ul>
+        </>
+      )}
     </section>
   );
 }
